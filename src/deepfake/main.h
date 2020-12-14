7d3beb9617d8c02a362349719429bfa2208849c0
d84-n1nj4 <g.h0st.snwbrd3r@gmail.com>
2020-12-14 15:02:12 -0500

    Fixed debugging issues (vimspector()
    )

--- src/deepfake/main.h
+++ src/deepfake/main.h
@@ -1,10 +1,35 @@
+;//
+//  main.h
+//  transfer-learning
+//
+//  Created by Kushashwa Ravi Shrimali on 15/08/19.
+//
+
+#ifndef main_h
 #define main_h
 
-#include "/home/mnuppnau/deepfake/include/deepfake/extract_images.h"
 #include <iostream>
 #include <torch/torch.h>
-#include <opencv2/opencv.hpp>
 #include <dirent.h>
+#include <torch/script.h>
+#include <opencv2/opencv.hpp>
+
+// Function to return image read at location given as type torch::Tensor
+// Resizes image to (224, 224, 3)
+torch::Tensor read_data(std::string location);
+
+// Function to return label from int (0, 1 for binary and 0, 1, ..., n-1 for n-class classification) as type torch::Tensor
+torch::Tensor read_label(int label);
+
+// Function returns vector of tensors (images) read from the list of images in a folder
+std::vector<torch::Tensor> process_images(std::vector<std::string> list_images);
+
+// Function returns vector of tensors (labels) read from the list of labels
+std::vector<torch::Tensor> process_labels(std::vector<int> list_labels);
+
+// Function to load data from given folder(s) name(s) (folders_name)
+// Returns pair of vectors of string (image locations) and int (respective labels)
+std::pair<std::vector<std::string>, std::vector<int>> load_data_from_folder(std::vector<std::string> folders_name);
 
 // Function to train the network on train data
 template<typename Dataloader>
@@ -39,3 +64,5 @@ public:
         return ds_size;
     };
 };
+
+#endif /* main_h */
